4df4e52fc086eda97e6f527086e155bc87de4b5c
Damian Kosowski <kosowski.d@gmail.com>
2016-05-25 21:51:44 +0200

    Add ability to find products and check products in stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,27 @@
 class Product
-	attr_reader :title
+	attr_reader :title, :price, :stock
 
 	@@products = []
 
 	def initialize(options={})
-		@title = options[:title]
+		@title, @price, @stock = options[:title], options[:price], options[:stock]
 		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	def self.find_by_title(title)
+		@@products.find { |product| product.title == title }
+	end
+
+	def self.in_stock
+		@@products.select { |product| product.in_stock? }
+	end
+
+	def in_stock?
+		!stock.zero?
 	end
 
 	private
